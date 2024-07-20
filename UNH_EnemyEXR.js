5a98e689b5c719f8bfc4fd4a336d09ea075bef0e
Ctelin Ajira <staleyjeffrey@gmail.com>
2024-07-20 18:02:17 -0400

    Update UNH_EnemyEXR.js

--- UNH_EnemyEXR.js
+++ UNH_EnemyEXR.js
@@ -6,7 +6,7 @@
 //=============================================================================
  /*:
  * @target MZ
- * @plugindesc [RPG Maker MZ] [Version 1.00] [Unhinged] [EnemyEXR]
+ * @plugindesc [RPG Maker MZ] [Version 1.01] [Unhinged] [EnemyEXR]
  * @author Unhinged Developer
  *
  * @help
@@ -17,17 +17,27 @@
  * This plugin just overrides the exp() function for enemies.  This is for the 
  * purposes of turning enemies' EXR into a multiplier to how much EXP they 
  * give.
+ *
+ * ============================================================================
+ * Changelog
+ * ============================================================================
+ * 1.01 - Updated plugin to include aliases
  */
 //=============================================================================
 
+const UNH_EnemyEXR = {};
+UNH_EnemyEXR.pluginName = 'UNH_EnemyEXR';
+
+UNH_EnemyEXR.Enemy_exp = Game_Enemy.prototype.exp;
 Game_Enemy.prototype.exp = function() {
-    let exp = this.enemy().exp;
+    let exp = UNH_EnemyEXR.Enemy_exp.call(this);
     exp *= this.exr;
     return exp;
 };
 
+UNH_EnemyEXR.Enemy_gold = Game_Enemy.prototype.gold;
 Game_Enemy.prototype.gold = function() {
-    let gold = this.enemy().gold;
+    let gold = UNH_EnemyEXR.Enemy_gold.call(this);
     return gold;
 };
 
