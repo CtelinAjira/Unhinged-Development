7935ebb956bb7cf93e86661c32960629e5f58804
Ctelin Ajira <staleyjeffrey@gmail.com>
2024-08-03 22:43:09 -0400

    Update UNH_AggroLevels.js

--- UNH_AggroLevels.js
+++ UNH_AggroLevels.js
@@ -3,12 +3,14 @@
 // UNH_AggroLevels.js
 //=============================================================================
 
+var Imported = Imported || {};
+
 //=============================================================================
  /*:
  * @target MZ
  * @orderAfter UNH_BleedStacks
  * @orderAfter VisuMZ_0_CoreEngine
- * @plugindesc [RPG Maker MZ] [Version 1.01] [Unhinged] [AggroLevels]
+ * @plugindesc [RPG Maker MZ] [Version 1.02] [Unhinged] [AggroLevels]
  * @author Unhinged Developer
  *
  * @param BaseAggro
@@ -41,6 +43,18 @@
  * @type number
  * @default 1
  *
+ * @param isTaunt
+ * @text Include Taunts
+ * @desc Should this plugin include taunts?
+ * @type boolean
+ * @default false
+ *
+ * @param isVoke
+ * @text Include Provocation
+ * @desc Should this plugin include provoke effects?
+ * @type boolean
+ * @default false
+ *
  * @help
  * ============================================================================
  * Plugin Description
@@ -71,6 +85,12 @@
  * - Adds X to the target's aggro (X is a JS formula)
  *   - user - the user
  *   - target - the target
+ * <unhTaunt>
+ * - Use for States
+ * - Forces all enemy attacks to target the battler afflicted with this state
+ * <unhProvoke>
+ * - Use for States
+ * - Forces all the afflicted battler's attacks to target the state's source
  * 
  * ============================================================================
  * New Functions
@@ -81,14 +101,14 @@
  * - returns the highest aggro total for the chosen unit
  * battler.unhAggroBase()
  * - returns the current aggro total for the battler
- * battler.unhAggroPlus()
- * action.unhAggroPlus()
+ * battler.unhAggroPlus(target)
+ * action.unhAggroPlus(target)
  * - returns the current total for (unhAggroPlus)
- * battler.unhAggroRate()
- * action.unhAggroRate()
+ * battler.unhAggroRate(target)
+ * action.unhAggroRate(target)
  * - returns the current total for (unhAggroRate)
- * battler.unhAggroFlat()
- * action.unhAggroFlat()
+ * battler.unhAggroFlat(target)
+ * action.unhAggroFlat(target)
  * - returns the current total for (unhAggroFlat)
  * battler.unhHpMult()
  * - returns the multiplier for aggro from HP damage
@@ -98,11 +118,11 @@
  * - resets the target's TGR to 100%
  * battler.unhSetAggro(X)
  * - sets the target's Base Aggro to X (Number)
- * battler.unhAddAggro(X)
- * battler.unhAddAggro(X, false)
+ * battler.unhAddAggro(target, X)
+ * battler.unhAddAggro(target, X, false)
  * - increases the target's Base Aggro by X (Number)
  *   - value is modified by plus/rate/flat
- * battler.unhAddAggro(X, true)
+ * battler.unhAddAggro(target, X, true)
  * - increases the target's Base Aggro by X (Number)
  *   - value is NOT modified by plus/rate/flat
  */
@@ -116,6 +136,96 @@ UNH_AggroLevels.HurtingAggro = Number(UNH_AggroLevels.parameters['HurtingAggro']
 UNH_AggroLevels.HealingAggro = Number(UNH_AggroLevels.parameters['HealingAggro'] || 0);
 UNH_AggroLevels.HPMult = Number(UNH_AggroLevels.parameters['HPMult'] || 0);
 UNH_AggroLevels.MPMult = Number(UNH_AggroLevels.parameters['MPMult'] || 0);
+UNH_AggroLevels.isTaunt = !!UNH_AggroLevels.parameters['isTaunt'];
+UNH_AggroLevels.isVoke = !!UNH_AggroLevels.parameters['isVoke'];
+
+if (!!UNH_AggroLevels.isTaunt) {
+  UNH_AggroLevels.BattlerManager_startAction_taunt = BattleManager.startAction;
+  BattleManager.startAction = function() {
+    const subject = this._subject;
+    const action = subject.currentAction();
+    const foes = action.opponentsUnit().aliveMembers();
+    UNH_AggroLevels.BattlerManager_startAction_taunt.call(this);
+    if (action.isForOpponent() && !action.isForEveryone()) {
+      const tauntTargets = [];
+      for (const member of foes) {
+        const tauntStates = JsonEx.makeDeepCopy(member.states()).filter(function() {
+          if (!state.meta) return false;
+          if (!state.meta.unhTaunt) return false;
+          return true;
+        });
+        if (tauntStates.length > 0) {
+          tauntTargets.push(member.index());
+        }
+      }
+      if (tauntTargets.length > 0) {
+        action.setTarget(tauntTargets[Math.randomInt(tauntTargets.length)]);
+      }
+    }
+  };
+}
+
+if (!!UNH_AggroLevels.isVoke) {
+  if (!Imported.VisuMZ_1_SkillsStatesCore) {
+    UNH_AggroLevels.Action_itemEffectAddAttackState = Game_Action.prototype.itemEffectAddAttackState;
+    Game_Action.prototype.itemEffectAddAttackState = function(target, effect) {
+      for (const stateId of this.subject().attackStates()) {
+        target.setStateOrigin(stateId, this.subject());
+      }
+      UNH_AggroLevels.Action_itemEffectAddAttackState.call(this, target, effect);
+    };
+
+    UNH_AggroLevels.Action_itemEffectAddNormalState = Game_Action.prototype.itemEffectAddNormalState;
+    Game_Action.prototype.itemEffectAddNormalState = function(target, effect) {
+      target.setStateOrigin(effect.dataId, this.subject());
+      UNH_AggroLevels.Action_itemEffectAddNormalState.call(this, target, effect);
+    };
+
+    Game_BattlerBase.prototype.getStateOrigin = function (stateId) {
+      this._stateOrigin = this._stateOrigin || {};
+      this._stateOrigin[stateId] = this._stateOrigin[stateId] || this;
+      return this._stateOrigin[stateId];
+    };
+
+    Game_BattlerBase.prototype.setStateOrigin = function (stateId, origin) {
+      this._stateOrigin = this._stateOrigin || {};
+      this._stateOrigin[stateId] = !!origin ? this : origin;
+    };
+
+    Game_BattlerBase.prototype.clearStateOrigin = function (stateId) {
+      this._stateOrigin = this._stateOrigin || {};
+      this._stateOrigin[stateId] = undefined;
+    };
+
+    Game_BattlerBase.prototype.clearAllStateOrigins = function () {
+      this._stateOrigin = {};
+    };
+  }
+
+  UNH_AggroLevels.BattlerManager_startAction_voke = BattleManager.startAction;
+  BattleManager.startAction = function() {
+    const subject = this._subject;
+    const action = subject.currentAction();
+    const foes = action.opponentsUnit().aliveMembers();
+    UNH_AggroLevels.BattlerManager_startAction_voke.call(this);
+    if (action.isForOpponent() && !action.isForEveryone()) {
+      const vokeOrigins = [];
+      const vokeStates = JsonEx.makeDeepCopy(subject.states()).filter(function(state) {
+        if (!state.meta) return false;
+        if (!state.meta.unhProvoke) return false;
+        return true;
+      });
+      for (const state of vokeStates) {
+        const origin = subject.getStateOrigin(state.id);
+        if (origin.isActor() === subject.isActor()) continue;
+        vokeOrigins.push(origin.index());
+      }
+      if (vokeOrigins.length > 0) {
+        action.setTarget(vokeOrigins[Math.randomInt(vokeOrigins.length)]);
+      }
+    }
+  };
+}
 
 Game_BattlerBase.prototype.unhDefaultAggro = function() {
   const user = this;
@@ -148,163 +258,151 @@ Game_Unit.prototype.unhMaxAggro = function() {
   return maxAggro;
 };
 
-Game_BattlerBase.prototype.unhAggroPlus = function() {
+Game_BattlerBase.prototype.unhAggroPlus = function(target) {
+  const user = this;
   let aggroPlus = 0;
   for (const state of this.states()) {
     if (!state.meta) continue;
     if (!state.meta.unhAggroPlus) continue;
-    if (typeof state.meta.unhAggroPlus !== 'number') continue;
-    aggroPlus += state.meta.unhAggroPlus;
+    aggroPlus += eval(state.meta.unhAggroPlus);
   }
   return aggroPlus;
 };
 
-Game_Enemy.prototype.unhAggroPlus = function() {
+Game_Enemy.prototype.unhAggroPlus = function(target) {
+  const user = this;
   let aggroPlus = Game_BattlerBase.prototype.unhAggroPlus.call(this);
   if (!!this.enemy().meta) {
     if (!!this.enemy().meta.unhAggroPlus) {
-      if (typeof this.enemy().meta.unhAggroPlus !== 'number') {
-        aggroPlus += this.enemy().meta.unhAggroPlus;
-      }
+      aggroPlus += eval(this.enemy().meta.unhAggroPlus);
     }
   }
   return aggroPlus;
 };
 
-Game_Actor.prototype.unhAggroPlus = function() {
+Game_Actor.prototype.unhAggroPlus = function(target) {
+  const user = this;
   let aggroPlus = Game_BattlerBase.prototype.unhAggroPlus.call(this);
   if (!!this.actor().meta) {
     if (!!this.actor().meta.unhAggroPlus) {
-      if (typeof this.actor().meta.unhAggroPlus !== 'number') {
-        aggroPlus += this.actor().meta.unhAggroPlus;
-      }
+      aggroPlus += eval(this.actor().meta.unhAggroPlus);
     }
   }
   for (const equip of this.equips()) {
     if (!equip.meta) continue;
     if (!equip.meta.unhAggroPlus) continue;
-    if (typeof equip.meta.unhAggroPlus !== 'number') continue;
-    aggroPlus += equip.meta.unhAggroPlus;
+    aggroPlus += eval(equip.meta.unhAggroPlus);
   }
   return aggroPlus;
 };
 
-Game_Action.prototype.unhAggroPlus = function() {
-  let aggroPlus = this.subject().unhAggroPlus();
+Game_Action.prototype.unhAggroPlus = function(target) {
+  const user = this.subject();
+  let aggroPlus = user.unhAggroPlus();
   const item = this.item();
   if (!!item.meta) {
     if (!!item.meta.unhAggroPlus) {
-      if (typeof item.meta.unhAggroPlus !== 'number'){
-        aggroPlus += item.meta.unhAggroPlus;
-      }
+      aggroPlus += eval(item.meta.unhAggroPlus);
     }
   }
   return aggroPlus;
 };
 
-Game_BattlerBase.prototype.unhAggroRate = function() {
+Game_BattlerBase.prototype.unhAggroRate = function(target) {
+  const user = this;
   let aggroRate = 1;
   for (const state of this.states()) {
     if (!state.meta) continue;
     if (!state.meta.unhAggroRate) continue;
-    if (typeof state.meta.unhAggroRate !== 'number') continue;
-    aggroRate *= state.meta.unhAggroRate;
+    aggroRate *= eval(state.meta.unhAggroRate);
   }
   return aggroRate;
 };
 
-Game_Enemy.prototype.unhAggroRate = function() {
+Game_Enemy.prototype.unhAggroRate = function(target) {
+  const user = this;
   let aggroRate = Game_BattlerBase.prototype.unhAggroRate.call(this);
   if (!!this.enemy().meta) {
     if (!!this.enemy().meta.unhAggroRate) {
-      if (typeof this.enemy().meta.unhAggroRate !== 'number') {
-        aggroRate *= this.enemy().meta.unhAggroRate;
-      }
+      aggroRate *= eval(this.enemy().meta.unhAggroRate);
     }
   }
   return aggroRate;
 };
 
-Game_Actor.prototype.unhAggroRate = function() {
+Game_Actor.prototype.unhAggroRate = function(target) {
+  const user = this;
   let aggroRate = Game_BattlerBase.prototype.unhAggroRate.call(this);
   if (!!this.actor().meta) {
     if (!!this.actor().meta.unhAggroRate) {
-      if (typeof this.actor().meta.unhAggroRate !== 'number') {
-        aggroRate *= this.actor().meta.unhAggroRate;
-      }
+      aggroRate *= eval(this.actor().meta.unhAggroRate);
     }
   }
   for (const equip of this.equips()) {
     if (!equip.meta) continue;
     if (!equip.meta.unhAggroRate) continue;
-    if (typeof equip.meta.unhAggroRate !== 'number') continue;
-    aggroRate *= equip.meta.unhAggroRate;
+    aggroRate *= eval(equip.meta.unhAggroRate);
   }
   return aggroRate;
 };
 
-Game_Action.prototype.unhAggroRate = function() {
-  let aggroRate = this.subject().unhAggroRate();
+Game_Action.prototype.unhAggroRate = function(target) {
+  const user = this.subject();
+  let aggroRate = user.unhAggroRate();
   const item = this.item();
   if (!!item.meta) {
     if (!!item.meta.unhAggroRate) {
-      if (typeof item.meta.unhAggroRate !== 'number'){
-        aggroRate *= item.meta.unhAggroRate;
-      }
+      aggroRate *= eval(item.meta.unhAggroRate);
     }
   }
   return aggroRate;
 };
 
-Game_BattlerBase.prototype.unhAggroFlat = function() {
+Game_BattlerBase.prototype.unhAggroFlat = function(target) {
+  const user = this;
   let aggroFlat = 0;
   for (const state of this.states()) {
     if (!state.meta) continue;
     if (!state.meta.unhAggroFlat) continue;
-    if (typeof state.meta.unhAggroFlat !== 'number') continue;
-    aggroFlat += state.meta.unhAggroFlat;
+    aggroFlat += eval(state.meta.unhAggroFlat);
   }
   return aggroFlat;
 };
 
-Game_Enemy.prototype.unhAggroFlat = function() {
+Game_Enemy.prototype.unhAggroFlat = function(target) {
+  const user = this;
   let aggroFlat = Game_BattlerBase.prototype.unhAggroFlat.call(this);
   if (!!this.enemy().meta) {
     if (!!this.enemy().meta.unhAggroFlat) {
-      if (typeof this.enemy().meta.unhAggroFlat !== 'number') {
-        aggroFlat += this.enemy().meta.unhAggroFlat;
-      }
+      aggroFlat += eval(this.enemy().meta.unhAggroFlat);
     }
   }
   return aggroFlat;
 };
 
-Game_Actor.prototype.unhAggroFlat = function() {
+Game_Actor.prototype.unhAggroFlat = function(target) {
+  const user = this;
   let aggroFlat = Game_BattlerBase.prototype.unhAggroFlat.call(this);
   if (!!this.actor().meta) {
     if (!!this.actor().meta.unhAggroFlat) {
-      if (typeof this.actor().meta.unhAggroFlat !== 'number') {
-        aggroFlat += this.actor().meta.unhAggroFlat;
-      }
+      aggroFlat += eval(this.actor().meta.unhAggroFlat);
     }
   }
   for (const equip of this.equips()) {
     if (!equip.meta) continue;
     if (!equip.meta.unhAggroFlat) continue;
-    if (typeof equip.meta.unhAggroFlat !== 'number') continue;
-    aggroFlat += equip.meta.unhAggroFlat;
+    aggroFlat += eval(equip.meta.unhAggroFlat);
   }
   return aggroFlat;
 };
 
-Game_Action.prototype.unhAggroFlat = function() {
-  let aggroFlat = this.subject().unhAggroFlat();
+Game_Action.prototype.unhAggroFlat = function(target) {
+  const user = this.subject();
+  let aggroFlat = user.unhAggroFlat();
   const item = this.item();
   if (!!item.meta) {
     if (!!item.meta.unhAggroFlat) {
-      if (typeof item.meta.unhAggroFlat !== 'number'){
-        aggroFlat += item.meta.unhAggroFlat;
-      }
+      aggroFlat += eval(item.meta.unhAggroFlat);
     }
   }
   return aggroFlat;
@@ -316,16 +414,28 @@ Game_BattlerBase.prototype.unhSetAggro = function(value) {
   this._unhAggroBase = Math.max(Math.round(value), 0);
 };
 
-Game_BattlerBase.prototype.unhAddAggro = function(value, ignoreRates) {
+Game_BattlerBase.prototype.unhAddAggro = function(target, value, ignoreRates) {
   if (value === undefined) value = 0;
   if (typeof value !== 'number') value = 0;
   if (!ignoreRates) {
-    value += this.unhAggroPlus();
-    value *= this.unhAggroRate();
-    value += this.unhAggroFlat();
+    value += this.unhAggroPlus(target);
+    value *= this.unhAggroRate(target);
+    value += this.unhAggroFlat(target);
   }
   value = Math.round(value);
   this.unhSetAggro(value + this.unhAggroBase());
+};
+
+Game_Action.prototype.unhAddAggro = function(target, value, ignoreRates) {
+  if (value === undefined) value = 0;
+  if (typeof value !== 'number') value = 0;
+  if (!ignoreRates) {
+    value += this.unhAggroPlus(target);
+    value *= this.unhAggroRate(target);
+    value += this.unhAggroFlat(target);
+  }
+  value = Math.round(value);
+  this.subject().unhSetAggro(value + this.unhAggroBase());
 };
 
 Game_BattlerBase.prototype.unhHpMult = function() {
@@ -360,10 +470,10 @@ Game_Action.prototype.apply = function(target) {
   UNH_AggroLevels.Action_apply.call(this);
   if (!!item.meta) {
     if (!!item.meta.unhAddUserAggro) {
-      user.unhAddAggro(eval(item.meta.unhAddUserAggro), false);
+      user.unhAddAggro(target, eval(item.meta.unhAddUserAggro), false);
     }
     if (!!item.meta.unhAddTargetAggro) {
-      target.unhAddAggro(eval(item.meta.unhAddTargetAggro), false);
+      target.unhAddAggro(target, eval(item.meta.unhAddTargetAggro), false);
     }
   }
 };
@@ -373,7 +483,8 @@ Game_Action.prototype.executeHpDamage = function(target, value) {
   let aggro = value;
   aggro *= this.subject().unhHpMult();
   aggro *= this.subject().unhDamageMult(aggro);
-  this.subject().unhAddAggro(Math.abs(aggro));
+  if (this.isDrain()) aggro *= 2;
+  this.unhAddAggro(target, Math.abs(aggro));
   UNH_AggroLevels.Action_executeHpDamage.call(this, target, value);
 };
 
@@ -382,26 +493,9 @@ Game_Action.prototype.executeMpDamage = function(target, value) {
   let aggro = value;
   aggro *= this.subject().unhMpMult();
   aggro *= this.subject().unhDamageMult(aggro);
-  this.subject().unhAddAggro(Math.abs(aggro));
+  if (this.isDrain()) aggro *= 2;
+  this.unhAddAggro(target, Math.abs(aggro));
   UNH_AggroLevels.Action_executeMpDamage.call(this, target, value);
-};
-
-UNH_AggroLevels.Action_gainDrainedHp = Game_Action.prototype.gainDrainedHp;
-Game_Action.prototype.gainDrainedHp = function(value) {
-  let aggro = value;
-  aggro *= this.subject().unhHpMult();
-  aggro *= this.subject().unhDamageMult(-aggro);
-  this.subject().unhAddAggro(Math.abs(aggro));
-  UNH_AggroLevels.Action_gainDrainedHp.call(this, value);
-};
-
-UNH_AggroLevels.Action_gainDrainedMp = Game_Action.prototype.gainDrainedMp;
-Game_Action.prototype.gainDrainedMp = function(value) {
-  let aggro = value;
-  aggro *= this.subject().unhMpMult();
-  aggro *= this.subject().unhDamageMult(-aggro);
-  this.subject().unhAddAggro(Math.abs(aggro));
-  UNH_AggroLevels.Action_gainDrainedMp.call(this, value);
 };
 
 UNH_AggroLevels.Action_itemEffectRecoverHp = Game_Action.prototype.itemEffectRecoverHp;
@@ -423,7 +517,7 @@ Game_Action.prototype.itemEffectRecoverHp = function(target, effect) {
   aggro = Math.floor(aggro);
   aggro *= this.subject().unhHpMult();
   aggro *= this.subject().unhDamageMult(-aggro);
-  this.subject().unhAddAggro(Math.abs(aggro));
+  this.unhAddAggro(target, Math.abs(aggro));
   UNH_AggroLevels.Action_itemEffectRecoverHp.call(this, target, effect);
 };
 
@@ -446,7 +540,7 @@ Game_Action.prototype.itemEffectRecoverMp = function(target, effect) {
   aggro = Math.floor(aggro);
   aggro *= this.subject().unhHpMult();
   aggro *= this.subject().unhDamageMult(-aggro);
-  this.subject().unhAddAggro(Math.abs(aggro));
+  this.unhAddAggro(target, Math.abs(aggro));
   UNH_AggroLevels.Action_itemEffectRecoverMp.call(this, target, effect);
 };
 
