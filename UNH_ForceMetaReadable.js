eb14a1ea659a0968d6f33990cf006c4f6fd53247
Ctelin Ajira <staleyjeffrey@gmail.com>
2024-11-20 18:15:27 -0500

    meep

--- UNH_ForceMetaReadable.js
+++ UNH_ForceMetaReadable.js
@@ -10,39 +10,37 @@
  * @author Unhinged Developer
  *
  * @help
+ * ============================================================================
+ * New Functions
+ * ============================================================================
+ *
+ * unit.unitMeta();
+ * - Returns an array with each member's metadata object
+ *
+ * battler.battlerMeta();
+ * - Returns an object with that battler's metadata
+ *
+ * item.itemMeta();
+ * - Returns an object with that item's metadata
+ *
+ * action.actionMeta();
+ * - Returns an object with that action's metadata, plus that of its user
+ *
+ * ============================================================================
+ * Notetags
+ * ============================================================================
+ *
+ * For enemies, you can use notetags to treat them as having equipment.  You 
+ * will need to tag them with <X:Y>, where X is the name of the equipment slot 
+ * within the database, and Y is the weapon or armor ID within the database.
+ *
+ * Giving the Dual Wield trait to a tagged enemy will change the shield tag to 
+ * another weapon.
  */
 //=============================================================================
 
 const UNH_ForceMetaReadable = {};
 UNH_ForceMetaReadable.pluginName = 'UNH_ForceMetaReadable';
-
-Object.defineProperty(Game_Actor.prototype, 'meta', {
-    get: function() {
-        return this.battlerMeta();
-    },
-    configurable: true
-});
-
-Object.defineProperty(Game_Enemy.prototype, 'meta', {
-    get: function() {
-        return this.battlerMeta();
-    },
-    configurable: true
-});
-
-Object.defineProperty(Game_Item.prototype, 'meta', {
-    get: function() {
-        return this.itemMeta();
-    },
-    configurable: true
-});
-
-Object.defineProperty(Game_Action.prototype, 'meta', {
-    get: function() {
-        return this.actionMeta();
-    },
-    configurable: true
-});
 
 UNH_ForceMetaReadable.concatMetadata = function(obj) {
   const meta = {};
@@ -57,6 +55,13 @@ UNH_ForceMetaReadable.concatMetadata = function(obj) {
   return meta;
 };
 
+Game_Unit.prototype.unitMeta = function() {
+  const meta = [];
+  for (const member of this.members()) {
+    meta.push(member.battlerMeta());
+  }
+};
+
 Game_BattlerBase.prototype.statesMeta = function() {
   const meta = Array($dataStates.length);
   for (const state of this.states()) {
@@ -69,7 +74,33 @@ Game_Enemy.prototype.battlerMeta = function() {
   const meta = {};
   meta['battler'] = UNH_ForceMetaReadable.concatMetadata(this.enemy());
   meta['class'] = {};
-  meta['equips'] = {};
+  const equipObj = {};
+  let equip;
+  for (let i = 1; i <= 7; i++) {
+    slotName = $gameSystem.equipTypes[i];
+    let eqpEval;
+    if (!this.enemy().meta) {
+      equipObj[slotName] = {};
+    } else if (!this.enemy().meta[slotName]) {
+      equipObj[slotName] = {};
+    } else {
+      try {
+        eqpEval = Function('return ' + String(this.enemy().meta[slotName]));
+        const eqpId = eqpEval();
+        if (i === 1) {
+          equip = $dataWeapons[eqpId];
+        } else if (i === 2 && this.isDualWield()) {
+          equip = $dataWeapons[eqpId];
+        } else {
+          equip = $dataArmors[eqpId];
+        }
+        equipObj[slotName] = equip.itemMeta();
+      } catch (e) {
+        equipObj[slotName] = {};
+      }
+    }
+  }
+  meta['equips'] = equipObj;
   meta['states'] = this.statesMeta();
   return meta;
 };
@@ -78,11 +109,20 @@ Game_Actor.prototype.battlerMeta = function() {
   const meta = {};
   meta['battler'] = UNH_ForceMetaReadable.concatMetadata(this.actor());
   meta['class'] = UNH_ForceMetaReadable.concatMetadata(this.currentClass());
-  equips = [];
-  for (const equip of this.equips()) {
-    equips[this.equips().indexOf(equip)] = equip.itemMeta();
+  const equipObj = {};
+  const equips = this.equips();
+  let equip;
+  let slotName;
+  for (let i = 0; i < equips.length; i++) {
+    equip = equips[i];
+    slotName = $gameSystem.equipTypes[i+1];
+    if (!!equip) {
+      equipObj[slotName] = equip.itemMeta();
+	} else {
+      equipObj[slotName] = {};
+    }
   }
-  meta['equips'] = equips;
+  meta['equipment'] = equipObj;
   meta['states'] = this.statesMeta();
   return meta;
 };
